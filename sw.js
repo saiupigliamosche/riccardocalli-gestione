9327f651eb6ae469562d8442e06bdc285cbec3b9
Saiu <riccardo.calli@gmail.com>
2026-09-20 10:50:07 +0200

    Bump cache for selected-date attendance

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="parkour-gestione-v4";
+const CACHE="parkour-gestione-v5";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
