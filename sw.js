cd2d4a933bef638752e3ab8749ab58788cba8293
Saiu <riccardo.calli@gmail.com>
2026-09-20 10:41:56 +0200

    Bump cache for Home calendar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="parkour-gestione-v2";
+const CACHE="parkour-gestione-v3";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
