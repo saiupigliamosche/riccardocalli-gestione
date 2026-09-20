3de1ea98043a1e3c05eb5fea6a905ea36f5b7b7d
Saiu <riccardo.calli@gmail.com>
2026-09-20 11:21:04 +0200

    Bump cache for lesson persistence

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="parkour-gestione-v8";
+const CACHE="parkour-gestione-v9";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
