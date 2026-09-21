dca3b86475146be1ea9e231f8174340e61fba191
Saiu <riccardo.calli@gmail.com>
2026-09-21 09:38:34 +0200

    Aggiorna cache gestionale v16

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="parkour-gestione-v15";
+const CACHE="parkour-gestione-v16";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
